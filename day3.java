fa7cb73b6ec73d1294d57b47a08c756da9f12b2d
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-01-04 19:55:17 +0530

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,19 @@
+//question link: https://www.interviewbit.com/problems/datatypes/
+
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String mystring1="InterviewBit";
+        String mystring2="Don't change the variable name";
+        int myint=11;
+        boolean myboolean=true;
+
+        System.out.println("String1 = " + mystring1);
+        System.out.println("String2 = " + mystring2);
+        System.out.println("int = " + myint);
+        System.out.println("boolean = " + myboolean);
+    }
+}
