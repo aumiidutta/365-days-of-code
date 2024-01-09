cccf171a2ad6b26c981a358d530400e884c91536
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-01-10 02:51:15 +0530

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,8 @@
+#question link: https://www.interviewbit.com/problems/input-and-output/
+
+def main():
+    user_input = input()
+    print('Hello' + ' ' + user_input)
+
+if __name__ == '__main__':
+    main()
