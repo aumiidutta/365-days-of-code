bf09bfbcf8e9db7382f1cea8d22b314398639956
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-01-14 10:39:07 +0530

    Create day13.py

---
+++ day13.py
@@ -0,0 +1,16 @@
+#question link: https://www.interviewbit.com/problems/variables-and-types/
+
+def main():
+    my_string1 = 'InterviewBit'
+    my_string2 = 'Don\'t change the variable name'
+    my_int = 11
+    my_float = 20.0
+    
+    print("String1: %s" % my_string1)
+    print("String2: %s" % my_string2)
+    print("Integer: %d" % my_int)
+    print("Float: %f" % my_float)
+    return 0
+
+if __name__ == '__main__':
+    main()
