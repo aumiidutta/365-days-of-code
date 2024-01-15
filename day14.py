3c159613f7256f51682af10d0a3b787ebba25baf
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-01-15 10:30:23 +0530

    Create day14.py

---
+++ day14.py
@@ -0,0 +1,15 @@
+#question link: https://www.interviewbit.com/problems/arithmetic-operators-on-numbers/
+
+def main():
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a//b)
+    print(a/b)
+    print(a%b)
+    print(a**b)
+
+if __name__ == '__main__':
+    main()
