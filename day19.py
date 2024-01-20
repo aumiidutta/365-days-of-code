649e5b5a7166fdabc469664700214ca3e1427995
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-01-20 20:37:25 +0530

    Create day19.py

---
+++ day19.py
@@ -0,0 +1,10 @@
+#question link: https://www.interviewbit.com/problems/loops-and-jump-statements/
+
+def main():
+    n = int(input())
+    for i in range(n):
+        if i%2!=0:
+            print(i)
+
+if __name__ == '__main__':
+    main()
