3cb1aa3f6bba4cff34f53da6631faeada9809ded
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-01-24 09:38:35 +0530

    Create day23.py

---
+++ day23.py
@@ -0,0 +1,11 @@
+#question link: https://www.interviewbit.com/problems/lists/
+
+
+def main():
+    names = []
+    names.append('Robin')
+    names.append('Aman')
+    names.append('Rahul')
+    print(names)
+if __name__ == '__main__':
+    main()
