bed3351d69ac397223b5019b44043d915e929081
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-02-02 18:51:16 +0530

    Create day32.py

---
+++ day32.py
@@ -0,0 +1,9 @@
+#question ink: https://www.interviewbit.com/problems/string-formatting/
+
+def main():
+    data = ("Robin", 10, "chocolates")
+    print("Hello %s. You are currently left with %d %s." % data)
+    #Hello Robin. You are currently left with 10 chocolates.
+
+if __name__ == '__main__':
+    main()
