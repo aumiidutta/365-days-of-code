7bb1ca66ea9492eaef323e6aa386f024fe90ceb3
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-02-28 10:37:39 +0530

    Create day58.py

---
+++ day58.py
@@ -0,0 +1,8 @@
+#question link: https://www.interviewbit.com/problems/max-min-05542f2f-69aa-4253-9cc7-84eb7bf739c4/
+
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        return max(A) + min(A)
