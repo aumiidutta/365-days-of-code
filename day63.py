03cf2ccfc972dc1b582ac61caecac050a51fe8b4
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-03-04 11:10:28 +0530

    Create day63.py

---
+++ day63.py
@@ -0,0 +1,9 @@
+#question link: https://www.interviewbit.com/problems/greatest-common-divisor/
+
+import math
+class Solution:
+	# @param A : integer
+	# @param B : integer
+	# @return an integer
+	def gcd(self, A, B):
+		return math.gcd(A, B)
