043f8da30117a8d038dc15e63f5e37b9f72c82fa
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-03-07 21:16:25 +0530

    Create day66.cs

---
+++ day66.cs
@@ -0,0 +1,10 @@
+//question link: https://www.interviewbit.com/problems/hello-world-csharp/
+
+
+using System;
+using System.IO;
+class MAIN  {
+    public static void Main(string[] args) {
+        System.Console.WriteLine("Hello, InterviewBit!");
+    }
+}
