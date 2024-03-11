7caaa2ea809a35d3cb7143cefdbce4a1c9ab0ba2
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-03-11 22:01:53 +0530

    Create day70.cs

---
+++ day70.cs
@@ -0,0 +1,11 @@
+//question link: https://www.interviewbit.com/problems/input-output-in-c/
+
+
+using System;
+using System.IO;
+class MAIN  {
+    public static void Main(string[] args) {
+        string str = Console.ReadLine();
+        Console.WriteLine("Hello" + " " + str);
+    }
+}
