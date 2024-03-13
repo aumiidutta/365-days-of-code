f7d9a706e3464ebef13eb5c15e85794a9c62de8d
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-03-13 20:36:35 +0530

    Create day72.cs

---
+++ day72.cs
@@ -0,0 +1,6 @@
+/*
+question link: https://www.interviewbit.com/problems/c-file-handling/
+
+Question: The __ class in the System.IO namespace is used to read from and write to any location in a file.
+Answer: FileStream
+*/
